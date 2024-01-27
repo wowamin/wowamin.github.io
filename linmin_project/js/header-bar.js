89714db0b9c38157da0859b95dfb0edbb6d20fd0
wowamin <wowamin125@gmail.com>
2024-01-27 15:25:10 +0800

    WoodMin
    重新規劃WEB

--- linmin_project/js/header-bar.js
+++ linmin_project/js/header-bar.js
@@ -3,7 +3,7 @@ nav.innerHTML = `
 <header class="container-fluid">
             <nav class="navbar navbar-expand-lg navbar-light" >
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html" style=" font-size: 2rem;"><img src="images/logo.svg" width="30%" style="margin-right: 10px;" alt="LinMin設計" />LinMin設計</a>
+                    <a class="navbar-brand" href="index.html" style=" font-size: 2rem;"><img src="images/logo.svg" width="30%" style="margin-right: 10px;" alt="WOODMIN設計" />WOODMIN設計</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -13,22 +13,12 @@ nav.innerHTML = `
                                 <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="https://simplemin125.wixsite.com/minlin125/about-4" target="_blank">關於我</a>
+                                <a class="nav-link" href="#" target="_blank">關於我</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> 平面設計 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="https://simplemin125.wixsite.com/minlin125/projects-2" target="_blank">LOGO設計</a></li>
-                                    <li><a class="dropdown-item" href="https://simplemin125.wixsite.com/minlin125/projects-2" target="_blank">名片設計</a></li>
-                                    <li><a class="dropdown-item" href="https://simplemin125.wixsite.com/minlin125/projects-2" target="_blank">DM設計</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> 攝影 </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="https://instagram.com/wowminwowho?igshid=OGQ5ZDc2ODk2ZA==" target="_blank">IG攝影集</a></li>
-                                    <li><a class="dropdown-item" href="#" target="_blank">商業攝影</a></li>
-                                    <li><a class="dropdown-item" href="#" target="_blank">活動攝影</a></li>
+                                    <li><a class="dropdown-item" href="https://riverwood2023.wixsite.com/minlin125/" target="_blank">商業設計</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item">
@@ -43,9 +33,6 @@ nav.innerHTML = `
                                     <li><a class="dropdown-item" href="https://super-m-777.1shop.tw" target="_blank">一頁式網頁設計練習</a></li>
                                     <li><a class="dropdown-item" href="https://cyvtc.com.tw/t0001/08-2" target="_blank">wordpress網頁課程練習</a></li>
                                 </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" target="_blank">生活小賣場</a>
                             </li>
                         </ul>
                         <form class="d-flex" role="search">
